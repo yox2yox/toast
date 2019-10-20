c7ea632a6c25b604db6cb9663eebe21c943da928
waradin <kuajiyoshi@gmail.com>
2019-10-20 23:27:25 +0900

    ToastPageの追加

---
+++ src/components/pages/ToastPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = {};
+
+class ToastPage extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default withStyles(styles)(ToastPage);
