f6753007a1a3c97266a52ca206bad82b840b19c8
waradin <kuajiyoshi@gmail.com>
2019-10-30 21:53:16 +0900

    ToastListを修正

--- src/components/ToastList.js
+++ src/components/ToastList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import CommentCard from './CommentCard';
 
